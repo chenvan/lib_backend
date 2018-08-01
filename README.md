5afb69aa38264bba1bb14a31b37d76fe6a6dba59
AWang <van1989@foxmail.com>
2018-08-01 12:41:20 +0800

    add PGroonga to support text search, improve table defition

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
+## TODO
+- book 表中, 加约束 now_number <= total_number
+- 对 fav, history 表应该对每个 uid 的数量进行限制: 写 sql 手工清除(暂时只想到这个办法)
+- 验证 PGroonga 对中文的搜索的作用
+
 ## 问题
 ### psql
-- 无法删除之前建立的 testdb
+- 无法删除之前建立的 testdb: 
 命令结尾没有加 ; 
+
+## 安装
+- 安装 PGroonga: [链接](https://pgroonga.github.io/install/)
--- migrations/20180731113436_setup.js
+++ migrations/20180731113436_setup.js
@@ -9,37 +9,49 @@ exports.up = function(knex, Promise) {
 
     knex.schema.createTable('book', function(table) {
       table.increments('bid')
-      table.string('isbn')
-      table.string('title').notNullable()
-      table.string('author')
+      // table.string('isbn')
+      table.text('title').notNullable()
+      table.text('author')
       table.text('cover_url')
       table.text('summary')
-      table.integer('all_number').notNullable()
-      table.integer('now_number').notNullable()
-
-      // title, author should use index that help text search
-    }),
+      // type column maybe should use enum
+      table.string('type')
+      table.integer('all_number').unsigned().notNullable()
+      table.integer('now_number').unsigned().notNullable()
+    })
+      .raw('CREATE INDEX pgroonga_title_index ON book USING pgroonga (title)')
+      .raw('CREATE INDEX pgroonga_author_index ON book USING pgroonga (author)')
+      ,
 
     knex.schema.createTable('fav', function(table) {
-
+      table.string('uid').notNullable().references('uid').inTable('user')
+      table.integer('bid').unsigned().notNullable().references('bid').inTable('book')
+      table.dateTime('add_time').notNullable().defaultTo(knex.raw('now()'))
+      table.unique(['uid', 'bid'])
     }),
     
     knex.schema.createTable('history', function(table) {
-
+      table.string('uid').notNullable().references('uid').inTable('user')
+      table.integer('bid').unsigned().notNullable().references('bid').inTable('book')
+      table.dateTime('add_time').notNullable().defaultTo(knex.raw('now()'))
+      table.unique(['uid', 'bid'])
     }),
 
     knex.schema.createTable('borrowing', function(table) {
-
+      table.string('uid').notNullable().references('uid').inTable('user')
+      table.integer('bid').unsigned().notNullable().references('bid').inTable('book')
+      table.dateTime('add_time').notNullable().defaultTo(knex.raw('now()'))
+      table.unique(['uid', 'bid'])
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('user'),
-    knex.schema.dropTable('book'),
     knex.schema.dropTable('fav'),
     knex.schema.dropTable('history'),
     knex.schema.dropTable('borrowing'),
+    knex.schema.dropTable('user'),
+    knex.schema.dropTable('book'),
   ])
 };
